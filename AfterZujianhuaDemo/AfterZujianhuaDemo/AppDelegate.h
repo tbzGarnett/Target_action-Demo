6c7e5c6f54d33a132cf85f062e44ce16590cfabe
taobingzhi <569355428@qq.com>
2019-03-13 17:28:42 +0800

    create project

---
+++ AfterZujianhuaDemo/AfterZujianhuaDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AfterZujianhuaDemo
+//
+//  Created by apple on 2019/3/13.
+//  Copyright © 2019 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AfterZujianhuaDemo/AfterZujianhuaDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AfterZujianhuaDemo
+//
+//  Created by apple on 2019/3/13.
+//  Copyright © 2019 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
